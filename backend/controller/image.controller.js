5b269f81756e9c2ec0c9cfbc364a118cd266f022
Muhammad Awais <kingyt6624@gmail.com>
2025-09-27 23:01:34 +0500

    feat(backend): implement image upload and delete with Cloudinary integration

--- backend/controller/image.controller.js
+++ backend/controller/image.controller.js
@@ -30,3 +30,56 @@ export const uploadImage = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+export const getAllImages = async (req, res) => {
+  try {
+    const allImages = await imageModel.find();
+
+    if (!allImages.length) {
+      return res.status(404).json({ message: "No image not found" });
+    }
+    res.status(200).json(allImages);
+  } catch (error) {
+    console.error("Error uploading image: ", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const updateImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { newTitle } = req.body;
+    const updateImage = await imageModel.findByIdAndUpdate(
+      id,
+      { title: newTitle },
+      { new: true }
+    );
+    if (updateImage === null) {
+      res.status(404).json({ message: "Image not found" });
+    }
+
+    res.status(200).json({ message: "Image Updated" });
+  } catch (error) {
+    console.error("Error uploading image: ", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const deleteImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedImage = await imageModel.findByIdAndDelete(id);
+
+    if (!deletedImage) {
+      return res.status(404).json({ message: "Image not found" });
+    }
+
+    await cloudinary.uploader.destroy(deletedImage.public_id);
+
+    res.status(200).json({ message: "Image deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting image: ", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- backend/routes/image.route.js
+++ backend/routes/image.route.js
@@ -3,5 +3,8 @@ import * as imageController from "../controller/image.controller.js";
 const router = Router();
 
 router.post("/upload", imageController.uploadImage);
+router.get("/allImages", imageController.getAllImages);
+router.put("/image/:id", imageController.updateImage);
+router.delete("/delete/:id", imageController.deleteImage);
 
 export default router;
